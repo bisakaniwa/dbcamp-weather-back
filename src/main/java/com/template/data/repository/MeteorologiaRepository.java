5374ee6124aea2ae0972ac97c05ce4f3757e1ee8
Bianca Sakaniwa <bianca.sakaniwa@dbserver.br>
2023-05-12 14:59:51 -0300

    refactor: removendo imports nao usados

--- src/main/java/com/template/data/repository/MeteorologiaRepository.java
+++ src/main/java/com/template/data/repository/MeteorologiaRepository.java
@@ -1,8 +1,6 @@
 package com.template.data.repository;
 
 import com.template.data.entity.MeteorologiaEntity;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
